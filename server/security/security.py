91bb477e1c409b6f99bf82a50d515ada5c4c7f50
Atanas Vasilev <avant.vasilev@gmail.com>
2024-12-17 23:12:35 +0200

    error handling improve + service sql schema

--- server/security/security.py
+++ server/security/security.py
@@ -2,6 +2,7 @@ from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordRequestForm
 from fastapi import Depends
+from fastapi import HTTPException, status
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="customer/access-token")
 password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -20,6 +21,8 @@ async def validate_customer_form_data(form_data: OAuth2PasswordRequestForm = Dep
 
 def _validate_customer_data(customer_name: str, password: str):
     if not customer_name or not customer_name.strip():
-        raise ValueError("Customer name cannot be empty or whitespace.")
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Customer name cannot be empty or whitespace.")
     if not password or len(password) < 8:
-        raise ValueError("Password must be at least 8 characters long.")
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Password must be at least 8 characters long.")
