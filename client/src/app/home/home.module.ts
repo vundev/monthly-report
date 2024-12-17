3d47b8e27a10beee0d61048c87187c046c821394
Atanas Vasilev <avant.vasilev@gmail.com>
2024-12-17 22:08:58 +0200

    add home page

--- client/src/app/home/home.module.ts
+++ client/src/app/home/home.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { CommonModule } from '@angular/common';
+import { ClarityModule } from '@clr/angular';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [HomeRoutingModule, CommonModule],
+  imports: [HomeRoutingModule, CommonModule, ClarityModule],
   providers: [],
   bootstrap: [HomeComponent],
 })
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,7 +1,11 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+import { ClarityIcons, homeIcon } from '@cds/core/icon';
+import '@cds/core/icon/register.js';
 import { AppModule } from './app/app.module';
 
+ClarityIcons.addIcons(homeIcon);
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
